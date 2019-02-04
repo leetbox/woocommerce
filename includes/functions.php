413d4f08f6d92dd8d659aef42eae2fd3abcf59c5
Ridhuan Hassan <ridhuan@matdespatch.com>
2019-02-04 15:02:04 +0800

    get value from form data sent by http post request

--- includes/functions.php
+++ includes/functions.php
@@ -26,36 +26,18 @@ function delyvaRequest() {
     } else if ($_GET['delyva'] == 'plugin_install') {
         header('Content-Type: application/json');
 
-        if (!$_GET['timestamp'] || !$_GET['hmac'] || !$_GET['user_id']) {
+        if (!$_POST['integration_id'] || !$_POST['api_key'] || !$_POST['user_id']) {
             die(json_encode(['error' => 'failed']));
         }
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, DELYVA_API_ENDPOINT . '/api/integration/woocommerce_install');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, [
-            'shop' => $_GET['shop'],
-            'timestamp' => $_GET['timestamp'],
-            'hmac' => $_GET['hmac'],
-            'user_id' => $_GET['user_id']
-        ]);
-        $results = json_decode(curl_exec($ch));
-        curl_close($ch);
+        update_option('delyva_integration_id', $_POST['integration_id']);
+        update_option('delyva_api_key', $_POST['api_key']);
+        update_option('delyva_user_id', $_POST['user_id']);
 
-        if (json_last_error() === JSON_ERROR_NONE && $results->integration_id) {
-            update_option('delyva_integration_id', $results->integration_id);
-            update_option('delyva_api_key', $results->api_key);
-            update_option('delyva_user_id', $results->user_id);
-
-            die(json_encode([
-                'integration_id' => $results->integration_id,
-                'api_key' => $results->api_key,
-                'user_id' => $results->user_id
-            ]));
-        } else {
-            die(json_encode(['error' => $results->integration_id]));
-        }
+        die(json_encode([
+            'integration_id' => $_POST['integration_id'],
+            'api_key' => $_POST['api_key'],
+            'user_id' => $_POST['user_id']
+        ]));
     }
 }
