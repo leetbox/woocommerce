07155faf3d2165d9bda271c20978770f6b077dba
Ridhuan Hassan <ridhuan@matdespatch.com>
2019-02-04 13:23:17 +0800

    rename matdespatch to delyva for general_setting.php

--- includes/hooks/general_setting.php
+++ includes/hooks/general_setting.php
@@ -2,46 +2,46 @@
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-  add_filter('woocommerce_general_settings', 'matdespatch_general_setting_field');
+  add_filter('woocommerce_general_settings', 'delyva_general_setting_field');
 
-  function matdespatch_general_setting_field($settings) {
+  function delyva_general_setting_field($settings) {
     $settings[] = array(
-      'name' => __( 'Matdespatch.com Settings', 'matdespatch' ),
+      'name' => __( 'Delyva.com Settings', 'delyva' ),
       'type' => 'title',
-      'id' => 'wc_general_settings_matdespatch_pricing_title'
+      'id' => 'wc_general_settings_delyva_pricing_title'
     );
 
     $settings[] = array(
-      'title'    	=> __( 'User Id', 'matdespatch' ),
-      'id'       	=> 'matdespatch_user_id',
+      'title'    	=> __( 'User Id', 'delyva' ),
+      'id'       	=> 'delyva_user_id',
       'type'     	=> 'hidden',
   //    'custom_attributes' => array('readonly' => 'readonly')
     );
 
     $settings[] = array(
-      'title'    	=> __( 'Api Key', 'matdespatch' ),
-      'id'       	=> 'matdespatch_api_key',
+      'title'    	=> __( 'Api Key', 'delyva' ),
+      'id'       	=> 'delyva_api_key',
       'type'     	=> 'hidden',
 //      'custom_attributes' => array('readonly' => 'readonly')
     );
 
     $settings[] = array(
-      'title'           => __( 'Integration Id', 'matdespatch' ),
-      'id'              => 'matdespatch_integration_id',
+      'title'           => __( 'Integration Id', 'delyva' ),
+      'id'              => 'delyva_integration_id',
       'type'            => 'hidden',
 //      'custom_attributes' => array('readonly' => 'readonly')
     );
 
     $settings[] = array(
-      'title'    	=> __( 'Price check', 'matdespatch' ),
-      'id'       	=> 'matdespatch_pricing_enable',
-      'desc'  	=> __( 'Show shipping method upon checkout', 'matdespatch' ),
+      'title'    	=> __( 'Price check', 'delyva' ),
+      'id'       	=> 'delyva_pricing_enable',
+      'desc'  	=> __( 'Show shipping method upon checkout', 'delyva' ),
       'type'     	=> 'checkbox',
       'default'	=> 'yes',
       //'desc_tip'	=> true,
     );
 
-    $settings[] = array( 'type' => 'sectionend', 'id' => 'wc_general_settings_matdespatch_pricing_section_end');
+    $settings[] = array( 'type' => 'sectionend', 'id' => 'wc_general_settings_delyva_pricing_section_end');
 
     return $settings;
   }
